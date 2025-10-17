77a479929235f936174043da007a881a0cc2a768
FGrimalt <jufagri@gmail.com>
2025-10-17 01:25:39 -0300

    Se agregaron validaciones a los metodos Post

--- api-articulo/Controllers/ArticuloController.cs
+++ api-articulo/Controllers/ArticuloController.cs
@@ -53,17 +53,59 @@ namespace api_articulo.Controllers
         }
 
         // POST: api/Articulo
-        public void Post([FromBody] ArticuloDTO articuloDTO)
+        [HttpPost]
+        [Route("api/Articulo")]
+        public HttpResponseMessage Post([FromBody] ArticuloDTO articuloDTO)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            Articulo nuevoArticulo = new Articulo();
-            nuevoArticulo.Codigo = articuloDTO.Codigo;
-            nuevoArticulo.Nombre = articuloDTO.Nombre;
-            nuevoArticulo.Descripcion = articuloDTO.Descripcion;
-            nuevoArticulo.IdMarca = new Marca { Id = articuloDTO.IdMarca };
-            nuevoArticulo.IdCategoria = new Categoria { Id = articuloDTO.IdCategoria };
-            nuevoArticulo.Precio = articuloDTO.Precio;
-            negocio.Agregar(nuevoArticulo);
+            try
+            {
+                if (articuloDTO == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No se ingresaron atributos a la solicitud.");
+
+                ArticuloNegocio negocioArt = new ArticuloNegocio();
+
+                if (string.IsNullOrWhiteSpace(articuloDTO.Codigo))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No se ingreso el código del artículo, el mismo es obligatorio.");
+
+                if (!negocioArt.Listar().Any(x => x.Codigo == articuloDTO.Codigo))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "El codigo ingresado ya existe "+ articuloDTO.Codigo +", ingrese otro codigo.");
+
+                if (string.IsNullOrWhiteSpace(articuloDTO.Nombre))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No se ingreso el nombre del artículo, el mismo es obligatorio.");
+
+                if (string.IsNullOrWhiteSpace(articuloDTO.Descripcion))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No se ingreso la descripcion del artículo, el mismo es obligatorio.");
+
+                if (articuloDTO.Precio == 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "El campo precio es obligario, debe ingresar un valor mayor a 0.");
+
+                if (articuloDTO.Precio < 0)
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "El articulo no puede tener un precio negativo, se debe ingresar un precio mayor a 0.");
+
+                
+                MarcaNegocio negocioMar = new MarcaNegocio();
+                if (!negocioMar.listar().Any(x => x.Id == articuloDTO.IdMarca))                    
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No existe una marca con ID: "+ articuloDTO.IdMarca);
+
+                CategoriaNegocio categoriaNeg = new CategoriaNegocio();
+                if (!categoriaNeg.listar().Any(x => x.Id == articuloDTO.IdCategoria))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No existe una categoría con ID: "+ articuloDTO.IdCategoria);
+
+                Articulo nuevoArticulo = new Articulo();
+                nuevoArticulo.Codigo = articuloDTO.Codigo;
+                nuevoArticulo.Nombre = articuloDTO.Nombre;
+                nuevoArticulo.Descripcion = articuloDTO.Descripcion;
+                nuevoArticulo.IdMarca = new Marca { Id = articuloDTO.IdMarca };
+                nuevoArticulo.IdCategoria = new Categoria { Id = articuloDTO.IdCategoria };
+                nuevoArticulo.Precio = articuloDTO.Precio;                
+                
+                negocioArt.Agregar(nuevoArticulo);
+                return Request.CreateResponse(HttpStatusCode.OK, "Articulo agregado correctamente");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError,"Ocurrió un error inesperado");
+            }            
         }
 
         // PUT: api/Articulo/5
--- api-articulo/Controllers/ImagenController.cs
+++ api-articulo/Controllers/ImagenController.cs
@@ -10,13 +10,48 @@ using System.Web.Http;
 namespace api_articulo.Controllers
 {
     public class ImagenController : ApiController
-    {      
+    {
 
         // POST api/Imagen
-        public void Post([FromBody] ImagenDTO imagenDTO)
+        [HttpPost]
+        [Route("api/Imagen")]
+        public HttpResponseMessage Post([FromBody] ImagenDTO imagenDTO)
         {
-            ImagenNegocio negocio = new ImagenNegocio();
-            negocio.Agregar(imagenDTO.ImagenUrl , imagenDTO.IdArticulo);
+            try
+            {
+                ArticuloNegocio negocioArt = new ArticuloNegocio();
+
+                if (imagenDTO == null)
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No se ingresaron atributos a la solicitud.");
+
+                if (!negocioArt.Listar().Any(x => x.Id == imagenDTO.IdArticulo))
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "El Id de articulo ingresado no existe: " + imagenDTO.IdArticulo + ", ingrese otro Id.");
+
+                if (imagenDTO.ImagenUrl == null || imagenDTO.ImagenUrl.Count == 0)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No se ingreso ningun atributo de Imagen, es obligatorio ingresar minimamente una url de imagen.");
+                }
+
+                if (imagenDTO.ImagenUrl.Count > 0) 
+                {
+                    foreach (string s in imagenDTO.ImagenUrl)
+                    {
+                        if (string.IsNullOrWhiteSpace(s))
+                        {
+                            return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Se intento ingresar una URL de imagen vacía, por favor de ingresar una url.");
+                        }
+                    }                    
+                }
+
+                ImagenNegocio negocioIMG = new ImagenNegocio();
+                negocioIMG.Agregar(imagenDTO.ImagenUrl, imagenDTO.IdArticulo);
+
+                return Request.CreateResponse(HttpStatusCode.OK, "Articulo agregado correctamente");
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Ocurrió un error inesperado");
+            }
 
         }
     }
